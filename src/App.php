4fa3e7ba3fa78e4d063c38de9da3bb062cef96e1
PXgamer <owzie123@gmail.com>
2017-03-05 15:28:06 +0000

    Reordered variables in App

--- src/App.php
+++ src/App.php
@@ -4,12 +4,11 @@ namespace pxgamer\DirSync;
 
 class App
 {
+    const HASH = "a75f22d3cd5aea";
     const RECEIVER_URL = "http://localhost/PlexCron/public/Server/";
 
     const CLIENT_NAME = "DirSyncClient";
     const SERVER_NAME = "DirSyncServer";
-
-    const HASH = "a75f22d3cd5aea";
 
     public static function json($content)
     {
