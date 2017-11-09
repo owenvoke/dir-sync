fabe0120a24d9602ed0bc7c119cd7f10122d2f43
pxgamer <owzie123@gmail.com>
2017-11-09 21:57:50 +0000

    Updated Client and Receiver classes to use new format

--- src/Receiver.php
+++ src/Receiver.php
@@ -2,25 +2,49 @@
 
 namespace pxgamer\DirSync;
 
+/**
+ * Class Receiver
+ */
 class Receiver
 {
+    /**
+     * @var App
+     */
+    private $app;
+    /**
+     * @var bool|string
+     */
     private $rootPath;
+    /**
+     * @var bool
+     */
     private $verified = false;
 
+    /**
+     * Receiver constructor.
+     */
     public function __construct()
     {
         $this->rootPath = realpath('..');
     }
 
+    /**
+     * @param $senderHash
+     * @return bool
+     */
     public function verify($senderHash)
     {
-        if (App::HASH === $senderHash) {
+        if ($this->app->getHash() === $senderHash) {
             $this->verified = true;
         }
 
         return $this->verified;
     }
 
+    /**
+     * @param $array
+     * @return array
+     */
     public function register($array)
     {
         if (!count($array)) {
@@ -32,6 +56,11 @@ class Receiver
         return ['status' => 'success'];
     }
 
+    /**
+     * @param array  $data
+     * @param string $fileName
+     * @return bool
+     */
     private function writeToFile($data = [], $fileName = "null")
     {
         $handle = fopen($fileName . '.txt', "w+");
@@ -39,9 +68,9 @@ class Receiver
             return false;
         }
         $json = (object)[
-            'last_updated_by' => App::CLIENT_NAME,
+            'last_updated_by' => $this->app->getClientName(),
             'last_updated_on' => time(),
-            'content' => $data
+            'content'         => $data
         ];
         fwrite($handle, json_encode($json));
         return true;
